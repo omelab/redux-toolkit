2ec066a107ba5e2b64d4636e5f774fff415db4ed
Abu Bakar Siddique <abu.bakar@apsissolutions.com>
2021-09-16 13:01:04 +0600

    initial porject

---
+++ help.js
@@ -0,0 +1,48 @@
+/* Blog Help */
+
+- ***https://blog.logrocket.com/using-typescript-with-redux-toolkit/ 
+- ****https://betterprogramming.pub/react-redux-saga-boilerplate-d2ca0c891ccd
+
+- https://github.com/justinlevi/react-typescript-router-redux-saga 
+- https://www.mattbutton.com/redux-made-easy-with-redux-toolkit-and-typescript/
+- https://dev.classmethod.jp/articles/how-to-use-redux-toolkit-react-typescript-projects/ 
+
+
+- https://scotch.io/tutorials/retrogames-library-with-node-react-and-redux-1-server-api-and-react-frontend
+- https://scotch.io/tutorials/build-a-retrogames-archive-with-node-js-react-redux-and-redux-saga-part2-redux-integration
+- https://scotch.io/tutorials/build-a-retrogames-archive-with-node-js-react-redux-and-redux-saga-part3-authentication
+- https://github.com/samuxyz/react-retrogames
+
+-*** https://start.jcolemorrison.com/react-and-redux-sagas-authentication-app-tutorial/
+
+- https://redux-saga.js.org/docs/basics/DeclarativeEffects
+- https://blog.isquaredsoftware.com/2019/10/redux-starter-kit-1.0/
+- https://cloudnweb.dev/2021/02/modern-react-redux-tutotials-redux-toolkit-login-user-registration/ 
+
+- https://www.bezkoder.com/react-redux-jwt-auth/
+- https://start.jcolemorrison.com/react-and-redux-sagas-authentication-app-tutorial/
+- https://dev.to/srivasthava12/react-authentication-app-using-redux-and-sagas-anp
+
+- https://fullstackfeed.com/react-and-redux-sagas-authentication-app-tutorial/
+
+
+- https://blog.bitsrc.io/complex-app-logic-with-redux-and-redux-saga-write-an-authentication-monitor-2f5672303d7d
+
+- *** https://nicedoc.io/xgrommx/awesome-redux
+- https://github.com/elliotaplant/redux-saga-auth
+
+- https://www.freecodecamp.org/news/login-using-react-redux-redux-saga-86b26c8180e/
+
+- https://redux-toolkit.js.org/introduction/getting-started
+
+/* cdn image using */
+- https://scotch.io/tutorials/using-an-image-cdn-to-speed-up-image-delivery-from-amazon-s3?utm_content=buffer0819c&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer
+
+/*Next js*/
+- https://kaloraat.com/articles/next-js-react-redux-authentication-tutorial
+
+
+/* Developer API*/
+- https://developers.line.biz/en/
+- https://cloudnweb.dev/2020/11/i-accidentally-wiped-database-in-production/
+- https://cloudnweb.dev/2021/01/building-nodejs-microservices-cloud-native-approach/
